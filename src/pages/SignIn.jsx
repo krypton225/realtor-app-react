00882c5e43dd2d7a32d99825e29bc4e14f060336
Krypton <k.mohamed.11.98@gmail.com>
2022-11-30 14:00:06 +0200

    ADD btns in sing in page & EDIT form inputs

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -6,6 +6,8 @@ import KeysImage from "../assets/images/keys.jpg";
 
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 
+import { FcGoogle } from "react-icons/fc";
+
 const SignIn = () => {
 
     const [showEye, setShowEye] = useState(false);
@@ -15,7 +17,7 @@ const SignIn = () => {
     const checkBrowserName = () => browserName.toLowerCase() === "edge";
 
     return (
-        <section className="container">
+        <section className="container px-6">
             <h3 className="mt-8 text-center text-lg sm:text-3xl capitalize font-semibold">sign in</h3>
 
             <div className="w-full mt-10 flex flex-col lg:flex-row justify-between items-center space-x-6">
@@ -23,14 +25,16 @@ const SignIn = () => {
                     <img src={KeysImage} className="w-full rounded-md" alt="Keys" />
                 </div>
 
-                <div className="w-1/2 mt-10 lg:mt-0">
-                    <form className="w-full flex flex-col justify-center items-center space-y-8">
-                        <input className="w-full py-1 px-2 border border-slate-400 rounded-md placeholder:capitalize"
-                            type="email" name="email" id="email" placeholder="enter email" />
+                <div className="w-full sm:w-3/4 md:w-1/2 mt-10 lg:mt-0">
+                    <form className="w-[97%] flex flex-col justify-center items-center space-y-8">
+                        <input type="email" className="w-full mx-auto py-1 px-2 border border-slate-400 rounded-md
+                            placeholder:capitalize focus:outline-none active:outline-none"
+                            name="email" id="email" placeholder="enter email" autoComplete="new-password" />
 
                         <div className="w-full relative cursor-pointer">
-                            <input className="w-full py-1 px-2 border border-slate-400 rounded-md placeholder:capitalize"
-                                type={`${showEye ? "text" : "password"}`} name="password" id="password" placeholder="enter password" />
+                            <input className="w-full mx-auto py-1 px-2 border border-slate-400
+                            rounded-md placeholder:capitalize focus:outline-none active:outline-none" name="password" id="password"
+                                type={`${showEye ? "text" : "password"}`} placeholder="enter password" autoComplete="new-password" />
                             {
                                 checkBrowserName() ? "" :
                                     <i className="absolute p-2 right-1 top-1/2 -translate-y-1/2 text-lg text-gray-600" onClick={changeEyeIcon}>
@@ -38,6 +42,20 @@ const SignIn = () => {
                                     </i>
                             }
                         </div>
+
+                        <button type="submit"
+                            className="w-full py-2 rounded-md bg-blue-500 text-white capitalize font-semibold cursor-pointer transition duration-[0.3s] hover:bg-blue-700">
+                            sign in
+                        </button>
+
+                        <div className="w-full relative before:content-[''] before:absolute before:h-[2px] before:w-[45%] before:bg-slate-800 before:top-1/2 before:left-0 after:content-[''] after:absolute after:h-[2px] after:w-[45%] after:bg-slate-800 after:top-1/2 after:right-0">
+                            <p className="text-center uppercase text-sm">or</p>
+                        </div>
+
+                        <button className="w-full py-2 rounded-md flex justify-center items-center bg-red-500 text-white capitalize font-semibold cursor-pointer transition duration-[0.3s] hover:bg-red-700">
+                            {<FcGoogle className="w-4 h-4 mr-4 bg-white rounded-full" />}
+                            sign up with google account
+                        </button>
                     </form>
                 </div>
             </div>
